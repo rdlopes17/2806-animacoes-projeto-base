da676f57207e2b5b0b69b23ee0cdd7d620699e80
rdlopes17 <rauldlopes@gmail.com>
2025-05-05 09:16:17 -0300

    desenv - pt 2 Implementando modulo animmations,  modificando estado atraves do trigger

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,17 +4,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
+import { state, style, trigger } from '@angular/animations';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
-  styleUrls: ['./lista-tarefas.component.css']
+  styleUrls: ['./lista-tarefas.component.css'],
+  animations: [
+    trigger('highlightedState', [
+      state(
+        //modulo de animacao
+        'default',
+        style({
+          // modulo de animacao
+          border: '2px solid #B2B6FF',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          border: '4px solid #B2B6FF',
+          filter: ' brightness(85%)',
+        })
+      ),
+    ]),
+  ],
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
   formAberto: boolean = false;
   categoria: string = '';
   validado: boolean = false;
+  indexTarefa = -1;
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
